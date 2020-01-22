18c9cdb14ed32fa7d525a4b2b5b59ed246709ed6
fabezio <fabezio@outlook.fr>
2020-01-22 06:52:54 +0100

    import des fichiers d'origine & début de construction

---
+++ components/Header.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+div
+  a.home(href='../index.html') fabezio.com
+  a.form(href='../form.html') inscription/connexion
+
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
---
+++ layouts/error.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+div
+  ul
+    li.menu
+      a(href='index.html') Accueil
+    li.menu
+      a(href='page1.html') Civilité
+    li.menu
+      a(href='page2.html') Culture
+    li.menu
+      a(href='page3.html') Evolution
+    li.menu
+      a(href='page4.html') Education
+    li.menu
+      a(href='page5.html') Informatique
+
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
